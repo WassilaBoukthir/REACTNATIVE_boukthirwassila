ec24c64e6ca27d0ba35032d21cea614589913202
Wassila Boukthir <159251466+WassilaBoukthir@users.noreply.github.com>
2024-12-12 17:12:55 +0100

    Update useProducts.ts

---
+++ src/api/useProducts.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+
+// Définir l'URL de l'API
+const PRODUCTS_API_URL = 'https://dummyjson.com/products';
+
+export const fetchProducts = async () => {
+  const { data } = await axios.get(PRODUCTS_API_URL);
+  return data.products;  // Retourne les produits
+};
+
+ Hook personnalisé pour récupérer les produits
+export const useProducts = () => {
+  return useQuery(['products'], fetchProducts);
+};
